54c06d83d864ed363a6f0d8d237004db5f22dfaf
Noah19cyber <alineborgescunha.abc@gmail.com>
2024-11-13 22:39:20 -0300

    Adicionando a configuração do Swagger e atualizando o DataContext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,16 +1,42 @@
-using ApiRotina.Models;  // Certifique-se de que isso esteja no topo do arquivo
+using ApiRotina.Models;
+using Microsoft.EntityFrameworkCore;
+using ApiRotina.Data;
 using Microsoft.EntityFrameworkCore;
 
- 
 
 namespace ApiRotina.Data
 {
+
     public class DataContext : DbContext
     {
-       
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-       
-        public DbSet<Tarefa> Tarefas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tarefa>().HasData
+                (
+            
+                new Tarefa() {Id = 1, nome = "Estudar", horario = 14, Concluida = true, descricao = "Estudar Matematica"},
+                new Tarefa() {Id = 2, nome = "Almoço", horario = 12, Concluida = false, descricao = "Almoçar"},
+                new Tarefa() {Id = 3, nome = "Estudar2", horario = 07, Concluida = true, descricao = "Estudar Portugues"},
+                new Tarefa() {Id = 4, nome = "jogar", horario = 16, Concluida = false, descricao = "jogar algum jogo"}
+                
+                );
+        }
+
+
     }
+    /* public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataContext>
+        {
+            public DataContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+                optionsBuilder.UseSqlServer(""); // Altere para a sua string de conexão
+
+                return new DataContext(optionsBuilder.Options);
+            }
+        }*/
 }
--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -7,19 +7,19 @@ namespace ApiRotina.Models
 public class Tarefa {
     public int Id { get; set; }
     public String nome { get; set; }
-    public String horario { get; set; }
-    public String descricao { get; set; }
-    public String Concluida { get; set;}
+    public int horario { get; set; }
+    public string descricao { get; set; }
+    public bool Concluida { get; set;}
 
     // Construtores, getters e setters
 
-    public Tarefa(int Id, String nome, String horario, String descricao) {
+    /*public Tarefa(int Id, String nome, int horario, string descricao, bool Concluida) {
         this.Id = Id;
         this.nome = nome;
         this.horario = horario;
         this.descricao = descricao;
         this.Concluida = Concluida;
-    }
+    }*/
 
     // Getters e Setters
 }
